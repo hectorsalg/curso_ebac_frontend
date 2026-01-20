9807b3ba9320681614cad7461cea93b19a394283
hectorsalg <hectorsalg@ufpi.edu.br>
2026-01-20 04:05:22 -0300

    Realizado exercício de TypeScript

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplicarDoisNumeros(a, b) {
+    return a * b;
+}
+function dizOla(nome) {
+    return 'Olá' + ` ${nome}`;
+}
+console.log(multiplicarDoisNumeros(8, 2));
+console.log(dizOla('Hector'));
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function multiplicarDoisNumeros(a: number, b: number): number {
+    return a * b;
+}
+
+function dizOla(nome: string): string {
+    return 'Olá' + ` ${nome}`;
+}
+
+console.log(multiplicarDoisNumeros(8, 2));
+console.log(dizOla('Hector'));
